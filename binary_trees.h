47cb8077f36bc99f23afe3bbb38ece4f38512af7
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-08-30 08:55:32 -0700

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -14,10 +14,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -25,7 +25,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-//task 0
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
