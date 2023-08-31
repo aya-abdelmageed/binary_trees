d75e10eff0cdaf235b52a4ffc9b2189306f47f25
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-08-31 04:22:46 -0700

    fix 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,14 +8,14 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t h1 = 0, h2 = 0;
+	size_t ans = 0, h1 = 0, h2 = 0;
 
 	if (tree)
 	{
 		h1 = (int)binary_tree_height(tree->left);
 		h2 = (int)binary_tree_height(tree->right);
 
-		return (h1 - h2);
+		ans = h1 - h2;
 	}
-	return (h1 - h2);
+	return (ans);
 }
