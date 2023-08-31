95c5b1429c516f5ca14ba11419ee844ef692465d
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-08-31 01:58:43 -0700

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,7 +11,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t l = 0, r = 0;
 
 	if (tree == NULL)
-		return (NULL);
+		return (0);
 
 	l = binary_tree_size(tree->left);
 	r = binary_tree_size(tree->right);
